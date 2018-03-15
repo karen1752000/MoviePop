4780d9cc6113cb9a7665d2d6cc9b116fa02cfe4d
Karen Mitchell <karen1752000@yahoo.com>
2018-03-15 15:54:41 -0400

    navbar created

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,16 @@
 //= require_tree .
 //= require jquery
 //= require jquery_ujs
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
