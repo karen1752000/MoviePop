54714d359cb14656388daea57168933dc61a30ba
Karen Mitchell <karen1752000@yahoo.com>
2018-03-19 16:36:57 -0400

    Turn Posters into buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,27 +14,3 @@
 //= require turbolinks
 //= require_tree .
 //= require jquery_ujs
-
-// $(document).ready(function(){
-//     // Activate Carousel
-//     $("#myCarousel").carousel();
-    
-//     // Enable Carousel Indicators
-//     $(".item1").click(function(){
-//         $("#myCarousel").carousel(0);
-//     });
-//     $(".item2").click(function(){
-//         $("#myCarousel").carousel(1);
-//     });
-//     $(".item3").click(function(){
-//         $("#myCarousel").carousel(2);
-//     });
-    
-//     // Enable Carousel Controls
-//     $(".left").click(function(){
-//         $("#myCarousel").carousel("prev");
-//     });
-//     $(".right").click(function(){
-//         $("#myCarousel").carousel("next");
-//     });
-// });
